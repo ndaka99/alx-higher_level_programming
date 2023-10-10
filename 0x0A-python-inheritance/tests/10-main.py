6855819d2b4690355df1563902d58d09de955eae
ndaka99 <ndakaerick@gmail.com>
2023-10-10 13:56:38 +0000

    latest updates

---
+++ 0x0A-python-inheritance/tests/10-main.py
@@ -0,0 +1,8 @@
+10-main.py
+#!/usr/bin/python3
+Square = __import__('10-square').Square
+
+s = Square(13)
+
+print(s)
+print(s.area())
---
+++ 0x0A-python-inheritance/tests/11-main.py
@@ -0,0 +1,8 @@
+11-main.py
+#!/usr/bin/python3
+Square = __import__('11-square').Square
+
+s = Square(13)
+
+print(s)
+print(s.area())
